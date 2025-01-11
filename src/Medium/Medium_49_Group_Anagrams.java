78100da55348b759a61bd7d2f87045496a3f4d74
Nazmul Hasan <nhnazmulhasan085@gmail.com>
2025-01-11 20:06:44 +0600

    LeetCode 1137 - String | LeetCode Daily

--- src/Medium/Medium_49_Group_Anagrams.java
+++ src/Medium/Medium_49_Group_Anagrams.java
@@ -19,9 +19,47 @@ public class Medium_49_Group_Anagrams {
             map.get(sortedWord).add(word);
         } return new ArrayList<>(map.values());
     }
+
+
+    // O(n*m) -> (length of strs * average size of strings in str)
+    public static List<List<String>> groupAnagramsOpt(String[] strs) {
+        // Key -> (aab) -> a2b1
+        // HashMap<String, List<String>> frequencyStringMap = new HashMap<>();
+
+        /* for (String str : strs) {
+            String key = getKey(str);
+            if (!frequencyStringMap.containsKey(key)) {
+                List<String> group = new ArrayList<>();
+                frequencyStringMap.put(key, group);
+            }
+            frequencyStringMap.get(key).add(str);
+        } */
+        HashMap<String, List<String>> groups = new HashMap<>();
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            groups.putIfAbsent(key, new ArrayList<>());
+            groups.get(key).add(str);
+        }
+        // return new ArrayList<>(frequencyStringMap.values());
+        return new ArrayList<>(groups.values());
+    }
+    private static String getKey(String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray())
+            freq[c-'a']++;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<26; i++) {
+            if (freq[i] > 0)
+                sb.append('a' + i).append(freq[i]);
+        }
+        return sb.toString();
+    }
     public static void main(String[] args) {
-        List<List<String>> answer = groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"});
-        for (List<String> group : answer)
-            System.out.println(Arrays.toString(group.toArray()));
+        System.out.println(groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
+        System.out.println(groupAnagrams(new String[]{""}));
+        System.out.println(groupAnagrams(new String[]{"a"}));
     }
 }
