3064f16651517da11d27863494237e16140700ed
Nazmul Hasan <nhnazmulhasan085@gmail.com>
2025-10-10 06:34:26 +0600

    Leetcode 1441 | Database

--- src/Database/1527. Patients With a Condition.sql
+++ src/Database/1527. Patients With a Condition.sql
@@ -1,11 +1,12 @@
-Create table If Not Exists Patients (patient_id int, patient_name varchar(30), conditions varchar(100))
+-- 1527. Patients With a Condition
+Create table If Not Exists Patients (patient_id int, patient_name varchar(30), conditions varchar(100));
+Truncate table Patients;
+insert into Patients (patient_id, patient_name, conditions) values ('1', 'Daniel', 'YFEV COUGH');
+insert into Patients (patient_id, patient_name, conditions) values ('2', 'Alice', '');
+insert into Patients (patient_id, patient_name, conditions) values ('3', 'Bob', 'DIAB100 MYOP');
+insert into Patients (patient_id, patient_name, conditions) values ('4', 'George', 'ACNE DIAB100');
+insert into Patients (patient_id, patient_name, conditions) values ('5', 'Alain', 'DIAB201');
 
-Truncate table Patients
-insert into Patients (patient_id, patient_name, conditions) values ('1', 'Daniel', 'YFEV COUGH')
-insert into Patients (patient_id, patient_name, conditions) values ('2', 'Alice', '')
-insert into Patients (patient_id, patient_name, conditions) values ('3', 'Bob', 'DIAB100 MYOP')
-insert into Patients (patient_id, patient_name, conditions) values ('4', 'George', 'ACNE DIAB100')
-insert into Patients (patient_id, patient_name, conditions) values ('5', 'Alain', 'DIAB201')
-
+-- 1527. Patients With a Condition
 SELECT * FROM Patients
-WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
